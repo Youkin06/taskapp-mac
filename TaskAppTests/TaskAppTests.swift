8a822fc3a92ff06b3b906a3807188df80c08a3c8
Youkin06 <yopi.games.tokyo@gmail.com>
2026-02-14 00:14:07 +0900

    Initial Commit

---
+++ TaskAppTests/TaskAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskAppTests.swift
+//  TaskAppTests
+//
+//  Created by ようへい on 2026/02/14.
+//
+
+import Testing
+@testable import TaskApp
+
+struct TaskAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
